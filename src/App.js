53e955d1263b634b16213d3213c6d7720a1a1cf8
buzzracharyeah rejan <rejandev@gmail.com>
2021-10-01 16:44:13 +0545

    generate index ensuring arrayOutOfBounds do not happen

--- src/App.js
+++ src/App.js
@@ -3,28 +3,41 @@ import Review from './Review';
 import data from './data';
 function App() {
   const [reviews, setReviews] = useState(data);
-  let [index, setIndex] = useState(0);
-  const prevPerson = () => {
-    if(index > 0) {
-      setIndex(prevValue => {
-        return prevValue - 1;
-      });
+  const [index, setIndex] = useState(0);
+  const checkNumber = number => {
+    if (number < 0 ) {
+      return reviews.length - 1;
     }
+    if(number > reviews.length - 1) {
+      return 0;
+    }
+    return number;
+
+  }
+  const prevPerson = () => {
+    setIndex(prevValue => {
+      let newValue = checkNumber(prevValue + 1);
+      return newValue;
+    });
   }; 
   const nextPerson = () => {
-    if(index < reviews.length - 1) {
-      setIndex(prevValue => {
-        return prevValue + 1;
-      });
-
-    }
+    setIndex(prevValue => {
+      let newValue = checkNumber(prevValue - 1);
+      return newValue;
+    });
   }
 
   const randomPerson = () => {
     const len = reviews.length - 1; 
     setIndex(prevValue => {
-      const index = Math.floor(Math.random() * (len - 0 + 1) + 0);
+      let index = Math.floor(Math.random() * (len - 0 + 1) + 0);
+      // console.log(`prevValue`, prevValue);
+      // console.log(`index`, index);
+      if(prevValue === index) {
+        return checkNumber(index + 1);
+      }
       return index;
+
     });
   } 
 
